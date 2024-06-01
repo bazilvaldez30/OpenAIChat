89bab93798441427e9ac144cd7e11cd20d2d9514
John Bazil Milan Valdez <bazilvaldez30@gmail.com>
2024-06-02 02:37:57 +0800

    test2

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -49,7 +49,7 @@ const chatAi = async (req, res) => {
       res.status(200).json({
         message: completion.choices[0].message,
         audioUrl: speechFile,
-        testfield: 'test',
+        testfield2: 'test2',
       })
     }
   } catch (error) {
